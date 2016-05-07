3ffb1574d980872c8635135dbbb39735a5905aff
Lizzie Paris <lzz.paris@gmail.com>
2016-05-07 14:32:28 -0700

    refactored app for functions, drawing most html via js so carousel-items are all siblings

--- js/app.js
+++ js/app.js
@@ -8,50 +8,10 @@ $(document).ready(function(){
 
 
 //init quiz
-var Question = {
-	questionText: "default",
-	answerOptions:[],
-	correctAnswer:"",
-	theirAnswer:"",
-	score:0,
-	storeAndCompare:function(input){
-		this.theirAnswer = input;
-		if(this.theirAnswer === this.correctAnswer){
-			this.score=1;
-			totalScore++;
-		}
-	}
-}
-
-var firstQ = Object.create(Question);
-var secondQ = Object.create(Question);
-var thirdQ = Object.create(Question);
-
-firstQ.questionText = "banana";
-firstQ.answerOptions= ["yellow","red","blue"];
-firstQ.correctAnswer= "yellow";
-secondQ.questionText = "apple";
-secondQ.answerOptions= ["green","purple","blue","black"];
-secondQ.correctAnswer= "green";
-thirdQ.questionText = "the third question";
-thirdQ.answerOptions= ["ta-da"];
+loadQuestions();
+drawQuiz();
 
 
-
-quiz.push(firstQ);
-quiz.push(secondQ);
-quiz.push(thirdQ);
-
-var questNum=0;
-quiz.forEach(function (quest){
-	var questID = "question-"+questNum; 
-	$(".question-content").append("<ul id=\""+questID+"\">");
-	$("#"+questID).append(
-		"<li class=\"question prompt\">"+quest.questionText+"</li>");
-	quest.answerOptions.forEach(function (a){
-		$("#"+questID).append("<li id=\""+a+"\"class=\"question answer\">"+a+"</li>");
-	})
-});
 
 //display first question
 $(".intro-start").click(function(){
@@ -93,3 +53,66 @@ $(".scorecard-restart").click(function(){
 
 
 });
+
+
+var Question = {
+	questionText: "default",
+	answerOptions:[],
+	correctAnswer:"",
+	theirAnswer:"",
+	score:0,
+	storeAndCompare:function(input){
+		this.theirAnswer = input;
+		if(this.theirAnswer === this.correctAnswer){
+			this.score=1;
+			totalScore++;
+		}
+	}
+}
+
+function loadQuestions() {
+
+var firstQ = Object.create(Question);
+var secondQ = Object.create(Question);
+var thirdQ = Object.create(Question);
+
+firstQ.questionText = "banana";
+firstQ.answerOptions= ["yellow","red","blue"];
+firstQ.correctAnswer= "yellow";
+secondQ.questionText = "apple";
+secondQ.answerOptions= ["green","purple","blue","black"];
+secondQ.correctAnswer= "green";
+thirdQ.questionText = "the third question";
+thirdQ.answerOptions= ["ta-da"];
+
+
+
+quiz.push(firstQ);
+quiz.push(secondQ);
+quiz.push(thirdQ);
+
+}
+
+function drawQuiz() {
+
+	$(".container").append("<section class=\"content intro carousel-item\" >"+
+		"<h1 class=\"intro-title\">know your fruit!</h1>"+
+		"<button class=\"intro-start\">start quiz</button>"+
+		"</section>");
+
+	quiz.forEach(function (quest, q){
+		var questID = "question-"+q; 
+		$(".container").append("<ul id=\""+questID+"\" class=\"carousel-item\">");
+		$("#"+questID).append(
+			"<li class=\"question prompt\">"+quest.questionText+"</li>");
+		quest.answerOptions.forEach(function (a){
+			$("#"+questID).append("<li id=\""+a+"\" class=\"question answer\">"+a+"</li>");
+			console.log("q is "+q);
+		})
+	});
+	$(".container").append("<section class=\"scorecard carousel-item\">"+
+		"<h2>you scored:</h2>"+
+		"<h3 class=\"scorecard-score\">0</h3>"+
+		"<button class=\"scorecard-restart\">play again?</button>"+
+		"</section>");
+}
