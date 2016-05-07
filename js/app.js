efa579a26376de4412a31fa58dbc77037afd1731
Lizzie Paris <lzz.paris@gmail.com>
2016-05-06 20:03:03 -0700

    html comment cleanup; app structure comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
+var totalScore = 0;
 
+//init quiz
 var Question = {
 	questionText: "default",
 	answerOptions:[],
@@ -11,6 +13,7 @@ var Question = {
 		this.theirAnswer = input;
 		if(this.theirAnswer === this.correctAnswer){
 			score=1;
+			totalScore++;
 		}
 	}
 }
@@ -21,13 +24,13 @@ var thirdQ = Object.create(Question);
 
 firstQ.questionText = "banana";
 firstQ.answerOptions= ["yellow","red","blue"];
+firstQ.correctAnswer= "blue";
 secondQ.questionText = "apple";
 secondQ.answerOptions= ["lksjdf;lajslgkjd;sj","lajksd;fjds;j;dsaj","laskjd;fjs;",";alskdjf;sjf"];
 thirdQ.questionText = "the third question";
 thirdQ.answerOptions= ["ta-da"];
 
 var quiz = [];
-
 
 quiz.push(firstQ);
 quiz.push(secondQ);
@@ -40,11 +43,30 @@ quiz.forEach(function (quest){
 	$("#"+questID).append(
 		"<li class=\"question prompt\">"+quest.questionText+"</li>");
 	quest.answerOptions.forEach(function (a){
-		$("#"+questID).append("<li class=\"question answer\">"+a+"</li>");
+		$("#"+questID).append("<li id=\""+a+"\"class=\"question answer\">"+a+"</li>");
 	})
-	questNum++;
-	// $(".question-content").append("</ul>");
+});
+
+//display first question
+$(".intro-start").click(function(){
+	//move intro offscreen
+	//move first question onscreen
+});
+
+//display next question
+$("li").click(function(){
+	//score
+	//if last question, update totalScore scorecard
+	//move question offscreen
+	//move next onscreen
+});
+
+//restart game
+$(".scorecard-restart").click(function(){
+	
 });
 
 
+
 });
+
